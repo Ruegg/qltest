7d5d1162c674b932ec1918ba2257f8f54fffaf2c
Ruegg <andre@ruegg.me>
2023-05-02 19:44:41 -0700

    initial

---
+++ main.py
@@ -0,0 +1,30 @@
+
+class ApiParam():
+    def __init__(self, name, required):
+        self.name = name
+        self.required = required
+
+class StringParam(ApiParam):
+    def __init__(self, name, required):
+        super().__init__(name, required)
+
+class api_method():
+    def __init__(self, uri, methods, params):
+        self.uri = uri
+        self.methods= methods
+        self.params = params
+    
+    def __call__(self, *argv):
+        pass
+
+
+@api_method(
+    "/v3/something/test",
+    methods=['PUT'],
+    params=[
+        StringParam("bucket", required=True)
+    ]
+)
+def v3_something(ctx, bucket):
+    print('something')
+    print(bucket)
